247a7fab3caef37cfe549b1c1a20b306303b92fe
varenichek <vasandra22@gmail.com>
2026-02-07 01:17:23 +0200

    Header UI rebuild

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,8 @@ import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { authService } from "../../services/authService/authService";
 import styles from "./Header.module.css";
-
-import searchIcon from "../../assets/icons/search.svg";
-import cartIcon from "../../assets/icons/cart.svg";
-import userIcon from "../../assets/icons/user.svg";
-import closeIcon from "../../assets/icons/burger-close.svg";
-import burgerIcon from "../../assets/icons/burger.svg";
-import homeIcon from "../../assets/icons/home.svg";
+import { useTheme } from '../../ThemeContext';
+import { ThemeToggle } from '../ThemeToggle/ThemeToggle';
 
 interface HeaderProps {
   enableSideMenu?: boolean;
@@ -25,6 +20,7 @@ export default function Header({
   const [searchValue, setSearchValue] = useState("");
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const { theme, toggleTheme } = useTheme();
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -75,11 +71,11 @@ export default function Header({
               onClick={onToggleMenu}
               aria-label={isMenuOpen ? "Close menu" : "Open menu"}
             >
-              <img
-                src={isMenuOpen ? closeIcon : burgerIcon}
-                alt={isMenuOpen ? "Close menu" : "Open menu"}
-                className={styles.burgerIcon}
-              />
+              {isMenuOpen ? (
+                <svg className={styles.burgerIcon} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 18 6M6 6l12 12"/></svg>
+              ) : (
+                <svg className={styles.burgerIcon} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/></svg>
+              )}
             </button>
           ) : (
             <button
@@ -87,11 +83,7 @@ export default function Header({
               onClick={() => navigate("/")}
               aria-label={"Go to homepage"}
             >
-              <img
-                src={homeIcon}
-                alt={"Go to homepage"}
-                className={styles.burgerIcon}
-              />
+              <svg className={styles.burgerIcon} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m3 12 2-2m0 0 7-7 7 7M5 10v10a1 1 0 0 0 1 1h3m10-11 2 2m-2-2v10a1 1 0 0 1-1 1h-3m-6 0a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1m-6 0h6"/></svg>
             </button>
           )}
           <div className={styles.logo} onClick={() => navigate("/")}>
@@ -101,12 +93,8 @@ export default function Header({
 
         <div className={styles.middleSection}>
           <div className={styles.searchWrapper}>
-            <img
-              src={searchIcon}
-              alt="Search"
-              className={styles.searchIcon}
-              onClick={() => handleSearch(searchValue)}
-            />
+            <svg className={styles.searchIcon} onClick={() => handleSearch(searchValue)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m21 21-6-6m2-5a7 7 0 1 1-14 0 7 7 0 0 1 14 0"/>
+            </svg>
             <input
               type="text"
               placeholder="Book title..."
@@ -120,13 +108,25 @@ export default function Header({
         </div>
 
         <div className={styles.userSection}>
+
+          <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
+
           <button
             className={styles.iconBtn}
             onClick={() => navigate("/orders")}
             title="Purchase Orders"
             aria-label="Purchase Orders"
           >
-            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAACXBIWXMAAAsTAAALEwEAmpwYAAAB10lEQVR4nO2bPy9DURiHn60TkgpWlfAFGAhD+RSoTcRgMiAdbB0k/sSImETMKBu6+bNiEb5JxZWTnCYnN1HR9L33dfs+yZubdOjvPU9v23vuPQcMwzCMlhgAFoB1YCOlctkl30ti9ADHwCcQKak6cAR0Sw++F3hVMOCf6gXISwq4jgXeA/vAVkrlsh9iPV1KDX4qCPkCFtHDku+p0d+ERMheEHCCPk6D/nYkAq6CgFn0MS/9NagFAUX0UQz6c722nZoJwAREJoCmAmaEL32nNQsoJHB5XPc5HS1gUKsA/OsVocveyi+/PSoEpIkJwARgAkhRQA64i01L21nufW99jkoBY8J/gY0a1Sog5z8hyTPgRvMZkDYmABOACeAfTYfXgJEsCSi0MBv8aGN/JoB/Nh3eBIazJiBNTAAmABOACcAEYAIwAQhQMwGYgMgEkKgA9xxwpcnzwMwLePd57tjRAt46VcAQsOqPHSngL5gAhAVUg4A59FEK+ruQCNgNAtyyVG2cBf1tSwRMxp7TuQXKWliO3WwdlwqqxoIe/cLktHaMuLPyKdbTOYLk/aaESGk9S2+YwG9LOfRL1iIl5Xo5ALpIkH6/RL2c4o6Rsu+hL8mBG4ZhkBW+AUMExDLHhRsmAAAAAElFTkSuQmCC" alt="purchase-order" className={styles.icon} />
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 17h4V5H2v12h3m15 0h2v-3.34a4 4 0 0 0-1.17-2.83L19 9h-5v8h1"/><circle cx="7.5" cy="17.5" r="2.5"/><circle cx="17.5" cy="17.5" r="2.5"/></svg>
+          </button>
+
+          <button
+            className={styles.iconBtn}
+            onClick={() => navigate("/transactions")}
+            title="Transactions"
+            aria-label="Transactions"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.183 24" width="21" height="24" fill="currentColor"><path d="M15.769 11.96h3.016v-.584c-.172-.639-.447-1.072-.803-1.335s-.816-.375-1.358-.372l-12.371.003a.551.551 0 0 1 0-1.102l12.356.002c.778-.007 1.463.169 2.027.587q.076.056.148.118v-.331c0-.515-.213-.985-.554-1.327a1.85 1.85 0 0 0-1.326-.553h-.399l-.051-.003-.194.729h-1.336l.793-2.974a1.253 1.253 0 0 1-.885-1.533l-4.41-1.182a1.253 1.253 0 0 1-1.532.886L7.593 7.793H6.215L8.313 0l9.359 2.508-.922 3.455h.155c.818 0 1.563.337 2.104.877.542.539.878 1.287.878 2.105v3.046a1.606 1.606 0 0 1 1.296 1.579v3.625c0 .462-.19.884-.493 1.186a1.7 1.7 0 0 1-.803.448v2.187c0 .818-.336 1.565-.877 2.106l-.034.032a2.97 2.97 0 0 1-2.07.845H2.982a2.97 2.97 0 0 1-2.106-.876A2.97 2.97 0 0 1 0 21.018V8.945c0-.821.335-1.567.876-2.107s1.286-.876 2.107-.876h1.739l.007.001L6.114.82l1.321.354L5.62 7.793H4.237l.196-.727H2.982c-.516 0-.986.212-1.327.553a1.87 1.87 0 0 0-.553 1.327v12.072c0 .516.213.986.553 1.327.342.342.812.554 1.327.554h13.923c.503 0 .963-.201 1.3-.527l.026-.027c.342-.342.554-.812.554-1.326v-2.142h-3.016a3.37 3.37 0 0 1-2.391-.994l-.057-.064a3.38 3.38 0 0 1-.936-2.328v-.146c0-.928.381-1.773.994-2.388l.006-.006a3.37 3.37 0 0 1 2.385-.991m-4.486-4.361a1.511 1.511 0 1 0 .782-2.92 1.511 1.511 0 0 0-.782 2.92m5.695 6.886.003.003a1.32 1.32 0 0 1-.001 1.862l-.003.003a1.324 1.324 0 0 1-1.863-.001l-.003-.003a1.324 1.324 0 0 1 .001-1.863l.003-.003a1.32 1.32 0 0 1 1.862.001"/></svg>
           </button>
 
           <button
@@ -135,7 +135,7 @@ export default function Header({
             title="Cart"
             aria-label="Shopping cart"
           >
-            <img src={cartIcon} alt="Cart" className={styles.icon} />
+            <svg width="24" height="24" viewBox="0 0 24 19.5" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M20.572 9H16.5V6H21zm-.643 4.5H16.5v-3h3.857zm-.429 3A1.5 1.5 0 0 1 18 18h-1.5v-3h3.214zM9 6h6v3H9zm0 4.5h6v3H9zM9 15h6v3H9zM7.5 9H3.214L3 6h4.5zm0 4.5H3.536l-.214-3H7.5zm0 4.5H5.25a1.5 1.5 0 0 1-1.5-1.5L3.643 15H7.5zM23.25 4.5h-4.894l1.519-3H22.5a.75.75 0 1 0 0-1.5h-3l-2.313 4.5H7.626L5.25 0h-3a.75.75 0 1 0 0 1.5h1.875l1.609 3H.75a.75.75 0 1 0 0 1.5h.75l.75 10.5a3 3 0 0 0 3 3H18a3 3 0 0 0 3-3L22.5 6h.75a.75.75 0 1 0 0-1.5"/></svg>
           </button>
 
           <div className={styles.accountWrapper} ref={dropdownRef}>
@@ -146,38 +146,31 @@ export default function Header({
               aria-label="User profile"
               aria-expanded={isDropdownOpen}
             >
-              <img src={userIcon} alt="User" className={styles.icon} />
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.875" d="M15.997 6.998a3.998 3.998 0 1 1-7.995 0 3.998 3.998 0 0 1 7.995 0M12 14.003A7 7 0 0 0 5.002 21h13.995A7 7 0 0 0 12 14.003"/></svg>
             </button>
 
             {isDropdownOpen && (
               <div className={styles.dropdown}>
                 {authService.getUserEmail() ? (
                   <>
-                  <div className={styles.dropdownEmail}>
-                    {authService.getUserEmail()}
-                  </div>
-                  <hr className={styles.divider} />
+                    <div className={styles.dropdownEmail}>
+                      {authService.getUserEmail()}
+                    </div>
+                    <hr className={styles.divider} />
 
-                  <button
-                    className={styles.dropdownItem}
-                    onClick={() => navigate("/transactions")}
-                  >
-                    Transactions
-                  </button>
+                    <button
+                      className={styles.dropdownItem}
+                      onClick={() => navigate("/promocodes")}
+                    >
+                      Promocodes
+                    </button>
 
-                  <button
-                    className={styles.dropdownItem}
-                    onClick={() => navigate("/promocodes")}
-                  >
-                    Promocodes
-                  </button>
-
-                  <button
-                    className={`${styles.dropdownItem} ${styles.logoutText}`}
-                    onClick={handleLogout}
-                  >
-                    Sign Out
-                  </button>
+                    <button
+                      className={`${styles.dropdownItem} ${styles.logoutText}`}
+                      onClick={handleLogout}
+                    >
+                      Sign Out
+                    </button>
                   </>
                 ) : (
                   <button
