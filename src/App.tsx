f4e50ed7b7b467fc531a8b2b31ee90d6aeec680b
varenichek <vasandra22@gmail.com>
2026-02-06 22:02:51 +0200

    Oauth + UI improvement

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,26 @@ import CartPage from "./pages/CartPage/CartPage";
 import OrdersPage from "./pages/OrdersPage/OrdersPage";
 import TransactionsPage from "./pages/TransactionsPage/TransactionsPage";
 import PromoCodePage from "./pages/PromoCodePage/PromoCodePage";
+import { authService } from "./services/authService/authService";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    if (params.get('auth_callback') === 'true') {
+      authService.authorizedFetch('/api/me')
+        .then(res => res.ok ? res.json() : null)
+        .then(userData => {
+          if (userData) {
+            authService.saveUserInfo(userData);
+            window.history.replaceState({}, document.title, window.location.pathname);
+            console.log("Авторизація через Google успішна!");
+          }
+        })
+        .catch(err => console.error("Помилка підтягування даних:", err));
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
