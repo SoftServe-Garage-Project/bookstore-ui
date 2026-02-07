f7c3b19a1a77252d734864bfead7978fd9115d94
varenichek <vasandra22@gmail.com>
2026-02-07 02:28:38 +0200

    fix + update

--- src/components/AuthForm/AuthFormWrapper.tsx
+++ src/components/AuthForm/AuthFormWrapper.tsx
@@ -2,8 +2,10 @@ import styles from './AuthFormWrapper.module.css';
 
 export default function AuthFormWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <div className={styles.wrapper}>
-      {children}
+    <div className={styles.pageContainer}>
+      <div className={styles.wrapper}>
+        {children}
+      </div>
     </div>
   );
 }
